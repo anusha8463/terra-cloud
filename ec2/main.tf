87dc02d03164241e5f90922342acd328c7e3cb2b
anusha8463 <146372237+anusha8463@users.noreply.github.com>
2023-12-20 18:11:26 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,6 @@
 #security group
 resource "aws_security_group" "webserver_access" {
-        name = "webserver_access"
+        name = "webserver_access_sg"
         description = "allow ssh and http"
 
         ingress {
