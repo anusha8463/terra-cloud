239919588d74910d115d6b50762ade213d782050
anusha8463 <146372237+anusha8463@users.noreply.github.com>
2023-12-20 17:24:10 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3"{
   bucket = "anusha-example-bucket"
-  key = "modules/backend/terraform.tfstate"
+  key = "terraform/backend/terraform.tfstate"
   region = "eu-north-1"
   dynamodb_table = "remote-statelocking"
   encrypt = true
